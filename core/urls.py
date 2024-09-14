2f306e7aa812b9dbc7ebb3c1312f222fd54a6457
shahspider@github.com <aq.syed007@gmail.com>
2024-09-15 02:15:28 +0500

    feat: add some models and templates & refactor views

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ app_name = "core"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.room, name='room')
+    path('room/<str:pk>/', views.room, name='room')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 def home(request):
-    return render(request, 'core/home.html')
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, 'core/home.html', context)
 
 
-def room(request):
-    return HttpResponse("Room page")
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, 'core/room.html', context)
