3f4b67cdbfb706aca348753d311fabce968393c2
shahspider@github.com <aq.syed007@gmail.com>
2024-09-20 00:50:26 +0500

    feat: part 1 of applying template theme

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('', views.home, name='home'),
+    path('profile/<str:pk>/', views.user_profile, name='user-profile'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>', views.update_room, name='update-room'),
--- core/views.py
+++ core/views.py
@@ -59,7 +59,7 @@ def home(request):
         )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {
         'rooms': rooms,
         'topics': topics,
@@ -67,6 +67,19 @@ def home(request):
         'room_messages': room_messages,
     }
     return render(request, 'core/home.html', context)
+
+def user_profile(request, pk):
+    user = User.objects.get(id=int(pk))
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        'user': user,
+        'rooms': rooms,
+        'room_messages': room_messages,
+        'topics': topics,
+    }
+    return render(request, "core/profile.html", context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
@@ -95,7 +108,9 @@ def create_room(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('core:home')
     return render(request, 'core/room_form.html', context)
 
